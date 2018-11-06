cf99132ac03816066f2172fd53175a8ec0e98582
Alexander little <alex@bloope.co>
2018-11-06 12:27:41 +0000

    Initial Commit

---
+++ Loot-iOS-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loot-iOS-Test
+//
+//  Created by Alex Little on 06/11/2018.
+//  Copyright © 2018 Alex Little. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
